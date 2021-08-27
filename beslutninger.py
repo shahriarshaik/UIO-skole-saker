624301c3016ca92f2d8383605d8c352879f34a4e
shahriarshaik <53064007+shahriarshaik@users.noreply.github.com>
2021-08-27 22:32:22 +0200

    Add files via upload

---
+++ beslutninger.py
@@ -0,0 +1,9 @@
+brus = input('vil du ha brus?:')
+
+if brus == "ja":
+    print('herhar du brus.')
+elif brus == "nei":
+    print('den er grei')
+else:
+    print('snakk norsk a')
+
---
+++ variabler.py
@@ -0,0 +1,20 @@
+print('hei strudent')
+
+navnet = input('vha er navnet ditt:')
+print(f'hei {navnet}, velkommen til mitt program')
+
+varEn = 10
+varTo = 20
+
+#print(varEn, varTo)
+print(f'''
+{varEn}
+{varTo}''')
+
+print(f'differanse: {varEn-varTo}')
+
+nynavnvariabel = input('yo jeg ble bedt om å sprrre om navnet ditt: ')
+
+sammen = navnet + ' og '+ nynavnvariabel
+print(f'dette er begge navnene samlet: {sammen}')
+print('jeg har hadde ikke lese oppgave 7 og jeg inkluderte allerede "og" i oppgave 6')
